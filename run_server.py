c9dd8395e82e3620e67f4ba54bf06a5f410879b8
EC2 Default User <ec2-user@ip-172-31-16-63.ec2.internal>
2023-06-26 05:52:02 +0000

    Updated for bloom

--- run_server.py
+++ run_server.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 app = FastAPI()
+import genmodel
+
+model = GenModel("bigscience/bloom-1b7")
 
 @app.get("/")
 async def main_fuction():
     return {"response":"The server is working."}
+
+@app.get("/")
+async def test_bloom(question:str):
+    model.run_prompt(str)
